be10354abae43bc827d6e4959313446db739c9aa
Alex Santos <devalexsantos@gmail.com>
2024-03-07 11:18:43 -0300

    fix: fix button call to action layout

--- src/app/corporate/_components/ButtonCallToAction.tsx
+++ src/app/corporate/_components/ButtonCallToAction.tsx
@@ -2,7 +2,7 @@ export default function ButtonCallToAction() {
   return (
     <a
       href="#solicitar"
-      className="bg-[#FFB930] hover:bg-[#ffb01c] transition-all text-[#305776] px-6 py-3 rounded-lg font-extrabold w-full max-w-4xl text-center text-xl mx-8"
+      className="bg-[#FFB930] hover:bg-[#ffb01c] transition-all text-[#305776] px-6 py-3 rounded-lg font-extrabold max-w-4xl text-center text-xl mx-8 md:w-full"
     >
       SOLICITAR UMA COTAÇÃO
     </a>

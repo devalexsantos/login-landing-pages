0da7ee4e4c8acbf66c25fbf99f5fc8623ac3e63d
Alex Santos <devalexsantos@gmail.com>
2024-03-11 14:32:16 -0300

    chore: change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
       <body className={lato.className}>
         {children}
         <Analytics />

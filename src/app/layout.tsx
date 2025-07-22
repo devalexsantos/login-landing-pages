a4f59351e13886cd355dd2ed1ebaa9d97bb1c08e
devalexsantos <devalexsantos@gmail.com>
2025-07-22 18:54:38 +0000

    chore: lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,46 @@
-import type { Metadata } from "next";
-import { Lato } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Lato } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
+import './globals.css'
 
 const lato = Lato({
-	subsets: ["latin"],
-	weight: ["100", "300", "400", "700", "900"],
-});
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', '900'],
+})
 
 export const metadata: Metadata = {
-	title: "Login Informática - Landing Pages",
-	description: "Páginas promocionais da Login Informática.",
-};
+  title: 'Login Informática - Landing Pages',
+  description: 'Páginas promocionais da Login Informática.',
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="pt-BR">
-			<head>
-				{/* Google Tag Manager */}
-				<Script
-					id="gtm"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  return (
+    <html lang="pt-BR">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-N6LJRRPP');`,
-					}}
-				/>
+          }}
+        />
 
-				{/* Meta Pixel */}
-				<Script
-					id="meta-pixel"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `!function(f,b,e,v,n,t,s)
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -50,33 +50,33 @@ export default function RootLayout({
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '3578381799135739');
             fbq('track', 'PageView');`,
-					}}
-				/>
+          }}
+        />
 
-				<link rel="icon" href="/favicon.svg" sizes="any" />
-			</head>
-			<body className={lato.className}>
-				<noscript>
-					{/* GTM noscript */}
-					<iframe
-						src="https://www.googletagmanager.com/ns.html?id=GTM-N6LJRRPP"
-						height="0"
-						width="0"
-						style={{ display: "none", visibility: "hidden" }}
-					></iframe>
-					{/* Meta Pixel noscript */}
-					<img
-						height="1"
-						width="1"
-						style={{ display: "none" }}
-						src="https://www.facebook.com/tr?id=3578381799135739&ev=PageView&noscript=1"
-						alt=""
-					/>
-				</noscript>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
+      <body className={lato.className}>
+        <noscript>
+          {/* GTM noscript */}
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N6LJRRPP"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+          {/* Meta Pixel noscript */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=3578381799135739&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
 
-				{children}
-				<Analytics />
-			</body>
-		</html>
-	);
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
 }
